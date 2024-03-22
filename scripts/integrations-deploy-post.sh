712012c46ed1ead76e158dc554a26da35c668d51
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-03-22 12:38:17 +0530

    Update integrations-deploy-post.sh

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "nodesymk" ]; then
+if [ "$DEPLOYMENT_GROUP_NAME" == "arjit-group" ]; then
     if [ -e /home/my-temp-dir/.env ]; then
         echo "Waiting for 2 minutes...."
         sleep 120

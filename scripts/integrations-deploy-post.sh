02d720edce68ca91da66efdd3c5d280f26c304e2
arjit547 <97424724+arjit547@users.noreply.github.com>
2025-12-27 23:25:05 +0530

    Update integrations-deploy-post.sh

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -1,34 +1,59 @@
+# #!/bin/bash
+
+# if [ "$DEPLOYMENT_GROUP_NAME" == "arjit-group" ]; then
+#     if [ -e /home/my-temp-dir/.env ]; then
+#         echo "Waiting for 2 minutes...."
+#         sleep 120
+#         cp -R /home/my-temp-dir/. /var/www/html
+#         rm -rf /home/my-temp-dir
+#         chown -R ubuntu:ubuntu /var/www/html
+#         cd /var/www/html
+#         npm install
+#         npm install -g pkg
+
+#         # Run npm build and check its exit code
+#         npm run build
+
+#         if [ $? -ne 0 ]; then
+#             echo "Error: npm run build failed."
+#         fi
+#     else
+#         cp -R /home/my-temp-dir/. /var/www/html
+#         rm -rf /home/my-temp-dir
+#         chown -R ubuntu:ubuntu /var/www/html
+#         cd /var/www/html
+#         npm install
+
+#         # Run npm build and check its exit code
+#         npm run build
+
+#         if [ $? -ne 0 ]; then
+#             echo "Error: npm run build failed."
+#         fi
+#     fi
+# fi
+
+
+
+
 #!/bin/bash
+set -e
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "arjit-group" ]; then
-    if [ -e /home/my-temp-dir/.env ]; then
-        echo "Waiting for 2 minutes...."
-        sleep 120
-        cp -R /home/my-temp-dir/. /var/www/html
-        rm -rf /home/my-temp-dir
-        chown -R ubuntu:ubuntu /var/www/html
-        cd /var/www/html
-        npm install
-        npm install -g pkg
+if [ "$DEPLOYMENT_GROUP_NAME" == "testnode" ]; then
 
-        # Run npm build and check its exit code
-        npm run build
+    echo "Deploying Node.js Backend..."
 
-        if [ $? -ne 0 ]; then
-            echo "Error: npm run build failed."
-        fi
-    else
-        cp -R /home/my-temp-dir/. /var/www/html
-        rm -rf /home/my-temp-dir
-        chown -R ubuntu:ubuntu /var/www/html
-        cd /var/www/html
-        npm install
+    cp -R /home/my-temp-dir/. /var/www/html
+    rm -rf /home/my-temp-dir
+    chown -R ubuntu:ubuntu /var/www/html
 
-        # Run npm build and check its exit code
-        npm run build
+    cd /var/www/html
+    npm install
 
-        if [ $? -ne 0 ]; then
-            echo "Error: npm run build failed."
-        fi
-    fi
+    pm2 delete node-app || true
+    pm2 start app.js --name node-app
+    pm2 save
+
+    sudo systemctl reload nginx
+    echo "Backend deployment completed."
 fi
